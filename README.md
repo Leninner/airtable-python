25f76520f25ad1a31e0d61e0ef2bd85291e743f9
Leninner <mazabandalenin180@gmail.com>
2021-10-27 17:28:21 -0500

    Update

--- README.md
+++ README.md
@@ -2,10 +2,20 @@
 
 Automatización con uso de Pyautogui, python, javascript, node.js, git.
 
-Ejecuta 
+Ejecuta
 
 ```bash
-npm install 
+npm install
 ```
+
 Para instalar todas las dependencias del proyecto.
 
+## Notas y aprendizajes del proyecto
+
+- Con la extensión **Code Runner** en archivos Python es mejor correr dando en la opción:
+  > Run python File in the Terminal
+- Se puede combinar muchos paquetes. En este proyecto combiné selenium y pyautogui.
+- El estrés puede afectar las relaciones personales, hay que cuidarse de eso.
+- Cuando sintamos que no podemos hacer algo, como un buen amigo me dijo: _Sal y descanza un momento y luego sígue adelante_
+- Utiliza la técnica del patito de hule.
+- Relativamente todo es posible con una gran pasión de por medio.
--- prueba.py
+++ prueba.py
@@ -1,25 +1,22 @@
 import pyautogui
 from selenium import webdriver
 from time import sleep
+import pyperclip as clipboard
 
 driver = webdriver.Chrome("C:/Users/USUARIO/Desktop/chromedriver.exe")
-driver.get("https://airtable.com/shr7TnsNHBrh1gdSo")
+driver.get("https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=106373116&keywords=marketing&location=ecuador")
 
-sleep(1)
+archivo = open("C:/Users/USUARIO/Desktop/leninner.txt", "r", encoding="utf8")
+texto = archivo.read()
+clipboard.copy(texto)
 
 pyautogui.click(200, 200)     # Double click the mouse.
-#pyautogui.moveTo(500, 500, duration=2, tween=pyautogui.easeInOutQuad)  # Use tweening/easing function to move mouse over 2 seconds.
+# #pyautogui.moveTo(500, 500, duration=2, tween=pyautogui.easeInOutQuad)  # Use tweening/easing function to move mouse over 2 seconds.
+
+pyautogui.hotkey('ctrl', "shift", 'j') 
 
 sleep(1)
 
-pyautogui.hotkey('ctrl', "shift", 'j') # Press the Ctrl-C hotkey combination.
+pyautogui.hotkey("ctrl", "v")
 
-sleep(1)
-
-pyautogui.write("lenin = 34")
-
-pyautogui.hotkey("enter")
-
-pyautogui.hotkey("alt", "tab")
-
-pyautogui.alert('This is the message to display.') # Make an alert box appear and pause the program until OK is clicked.
+archivo.close()
