26daba81901ce7610608e74e7e897e857d38c3db
Leninner <mazabandalenin180@gmail.com>
2021-10-26 20:51:30 -0500

    U´date

--- prueba.py
+++ prueba.py
@@ -1,5 +1,25 @@
 import pyautogui
+from selenium import webdriver
+from time import sleep
 
-pyautogui.click(1360, 10, 2)
-print(pyautogui.size())
-pyautogui.alert("¡Hola, mundo!", "Desde PyAutoGUI")
+driver = webdriver.Chrome("C:/Users/USUARIO/Desktop/chromedriver.exe")
+driver.get("https://airtable.com/shr7TnsNHBrh1gdSo")
+
+sleep(1)
+
+pyautogui.click(200, 200)     # Double click the mouse.
+#pyautogui.moveTo(500, 500, duration=2, tween=pyautogui.easeInOutQuad)  # Use tweening/easing function to move mouse over 2 seconds.
+
+sleep(1)
+
+pyautogui.hotkey('ctrl', "shift", 'j') # Press the Ctrl-C hotkey combination.
+
+sleep(1)
+
+pyautogui.write("lenin = 34")
+
+pyautogui.hotkey("enter")
+
+pyautogui.hotkey("alt", "tab")
+
+pyautogui.alert('This is the message to display.') # Make an alert box appear and pause the program until OK is clicked.
