37935df6e2aeb17549db58f9df19e7cedca60b0f
Lenin Mazabanda <mazabandalenin180@gmail.com>
2021-11-19 10:52:02 -0500

    Update sendJobs.js

--- src/sendJobs.js
+++ src/sendJobs.js
@@ -1,5 +1,5 @@
 const Airtable = require('airtable');
-const base = new Airtable({ apiKey: 'key6tZIQa1Dih72xX' }).base('app5fCgRbl0Uk9ToJ');
+const base = new Airtable({ apiKey: '*************' }).base('****************');
 
 // NOTE: Método muchísimo más veloz para añadir empleos a Airtable
 
