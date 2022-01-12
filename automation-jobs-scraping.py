cae1becf93b3d73a5e015c5c332544075bed7ad2
rpbenavides22 <rpbenavides22@gmail.com>
2022-01-12 17:25:29 -0500

    Update PB

--- automation-jobs-scraping.py
+++ automation-jobs-scraping.py
@@ -7,33 +7,33 @@ import os
 links = [
     "https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=106373116&keywords=direcci%C3%B3n&location=Ecuador",
     "https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=106373116&keywords=marketing&location=ecuador",
-    "https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=106373116&keywords=tecnolog%C3%ADa&location=ecuador"
-    "https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=106373116&keywords=ventas&location=Ecuador",
-    "https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=106373116&keywords=contabilidad&location=Ecuador",
-    "https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=106373116&keywords=finanzas&location=Ecuador",
-    "https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=106373116&keywords=log%C3%ADstica&location=Ecuador",
-    "https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=106373116&keywords=developer&location=Ecuador",
-    "https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=106373116&keywords=produccion%20audiovisual&location=Ecuador",
-    "https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=106373116&keywords=produccion&location=Ecuador",
-    "https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=106373116&keywords=innovaci%C3%B3n&location=Ecuador",
-    "https://ec.jooble.org/SearchResult?date=8&ukw=tecnolog%C3%ADa",
-    "https://ec.jooble.org/SearchResult?date=8&ukw=developer",
-    "https://ec.jooble.org/SearchResult?date=8&p=2&ukw=ventas",
-    "https://www.multitrabajos.com/empleos-publicacion-hoy.html",
-    "https://www.computrabajo.com.ec/empleos-de-informatica-y-telecom-hoy",
-    "https://www.computrabajo.com.ec/empleos-de-recursos-humanos-hoy",
-    "https://www.computrabajo.com.ec/empleos-de-marketing-y-ventas-hoy",
-    "https://www.computrabajo.com.ec/empleos-de-atencion-a-clientes-hoy",
-    "https://www.computrabajo.com.ec/empleos-de-administracion-y-oficina-hoy",
-    "https://www.computrabajo.com.ec/empleos-de-cientifico-y-investigacion-hoy",
-    "https://www.computrabajo.com.ec/empleos-de-arte-y-diseno-y-medios-hoy",
-    "https://www.computrabajo.com.ec/empleos-de-almacen-logistica-hoy"
+    # "https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=106373116&keywords=tecnolog%C3%ADa&location=ecuador"
+    # "https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=106373116&keywords=ventas&location=Ecuador",
+    # "https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=106373116&keywords=contabilidad&location=Ecuador",
+    # "https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=106373116&keywords=finanzas&location=Ecuador",
+    # "https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=106373116&keywords=log%C3%ADstica&location=Ecuador",
+    # "https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=106373116&keywords=developer&location=Ecuador",
+    # "https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=106373116&keywords=produccion%20audiovisual&location=Ecuador",
+    # "https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=106373116&keywords=produccion&location=Ecuador",
+    # "https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=106373116&keywords=innovaci%C3%B3n&location=Ecuador",
+    # "https://ec.jooble.org/SearchResult?date=8&ukw=tecnolog%C3%ADa",
+    # "https://ec.jooble.org/SearchResult?date=8&ukw=developer",
+    # "https://ec.jooble.org/SearchResult?date=8&p=2&ukw=ventas",
+    # "https://www.multitrabajos.com/empleos-publicacion-hoy.html",
+    # "https://www.computrabajo.com.ec/empleos-de-informatica-y-telecom-hoy",
+    # "https://www.computrabajo.com.ec/empleos-de-recursos-humanos-hoy",
+    # "https://www.computrabajo.com.ec/empleos-de-marketing-y-ventas-hoy",
+    # "https://www.computrabajo.com.ec/empleos-de-atencion-a-clientes-hoy",
+    # "https://www.computrabajo.com.ec/empleos-de-administracion-y-oficina-hoy",
+    # "https://www.computrabajo.com.ec/empleos-de-cientifico-y-investigacion-hoy",
+    # "https://www.computrabajo.com.ec/empleos-de-arte-y-diseno-y-medios-hoy",
+    # "https://www.computrabajo.com.ec/empleos-de-almacen-logistica-hoy"
 ]
 
 for link in links:  
-    driver = webdriver.Chrome("C:/Users/USUARIO/Desktop/chromedriver.exe")
+    driver = webdriver.Chrome("/Users/macbookpro/Desktop/chromedriver")
     driver.get(link)
-    archivo = open("C:/Users/USUARIO/Desktop/Automation/scrapingJobs.js", "r", encoding="utf8")
+    archivo = open("/Users/macbookpro/Desktop/airtable-python/scrapingJobs.js", "r", encoding="utf8")
     texto = archivo.read()
     clipboard.copy(texto)
     pyautogui.click(200, 200)
@@ -43,22 +43,22 @@ for link in links:
     pyautogui.hotkey("enter")
     sleep(1)
     pyautogui.hotkey("alt", "tab")
-    copiar = open("C:/Users/USUARIO/Desktop/Automation/src/jobs.js", "a+", encoding="utf8")
+    copiar = open("/Users/macbookpro/Desktop/airtable-python/src/jobs.js", "a+", encoding="utf8")
     copiar.write(clipboard.paste())
     copiar.write(",")
     archivo.close()
     driver.quit()
 
-copiar = open("C:/Users/USUARIO/Desktop/Automation/src/jobs.js", "a+", encoding="utf8")
+copiar = open("/Users/macbookpro/Desktop/airtable-python/src/jobs.js", "a+", encoding="utf8")
 copiar.write("]\n")
-moreCode = open("C:/Users/USUARIO/Desktop/Automation/utils/moreCode.js", "r", encoding="utf8")
+moreCode = open("/Users/macbookpro/Desktop/airtable-python/utils/moreCode.js", "r", encoding="utf8")
 texto = moreCode.read()
 clipboard.copy(texto)
 copiar.write(clipboard.paste())
 moreCode.close()
 copiar.close()
 
-RUTA_ARCHIVO = "C:/Users/USUARIO/Desktop/Automation/src/jobs.js"
+RUTA_ARCHIVO = "/Users/macbookpro/Desktop/airtable-python/src/jobs.js"
 os.system(f'start {os.path.realpath(RUTA_ARCHIVO)}')
 sleep(5)
 pyautogui.click(500, 200)
